fcebea4e51741b84236f6ee508e05c958b972a9d
Lukasz Niedzwiedz <dziwiedz@gmail.com>
2018-08-06 13:21:14 +0200

    adds api router

---
+++ UsersApp/Domain/Network/ApiRouter.swift
@@ -0,0 +1,14 @@
+//
+//  ApiRouter.swift
+//  Domain
+//
+//  Created by Łukasz Niedźwiedź on 06/08/2018.
+//  Copyright © 2018 Niedzwiedz. All rights reserved.
+//
+
+import Foundation
+
+public final class ApiRouter {
+    static let githubBaseUrl: String = "https://api.github.com"
+    static let dailymotionBaseUrl: String = "https://api.dailymotion.com"
+}
