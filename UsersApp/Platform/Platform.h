baeb06ab390678f97d4a79f5df62f34febe1d739
Lukasz Niedzwiedz <dziwiedz@gmail.com>
2018-08-06 13:01:31 +0200

    creates modules

---
+++ UsersApp/Platform/Platform.h
@@ -0,0 +1,19 @@
+//
+//  Platform.h
+//  Platform
+//
+//  Created by Łukasz Niedźwiedź on 06/08/2018.
+//  Copyright © 2018 Niedzwiedz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Platform.
+FOUNDATION_EXPORT double PlatformVersionNumber;
+
+//! Project version string for Platform.
+FOUNDATION_EXPORT const unsigned char PlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Platform/PublicHeader.h>
+
+
